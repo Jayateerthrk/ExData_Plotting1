f6c0640bf7f539e30b090943608baea2376d1c03
Jayateerth <Jayateerthk@gmail.com>
2017-09-06 14:52:37 +0530

    Plot assignment

---
+++ plot1.R
@@ -0,0 +1,11 @@
+#Read data from Text file
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+#Subset based on date
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,12 @@
+#Read data from Text file
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+#Subset based on date
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#convert the date
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
